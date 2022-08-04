5b8efd92b4a80bc3e799d5acb47abba77f18e5ca
teslaluv <109180406+teslaluv@users.noreply.github.com>
2022-08-05 01:23:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,20 @@ import random
 
 app = Flask(__name__)
 
-# list of cat images
+# List of images
 images = [
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26388-1381844103-11.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr01/15/9/anigif_enhanced-buzz-31540-1381844535-8.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26390-1381844163-18.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/10/anigif_enhanced-buzz-1376-1381846217-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/9/anigif_enhanced-buzz-3391-1381844336-26.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/10/anigif_enhanced-buzz-29111-1381845968-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/9/anigif_enhanced-buzz-3409-1381844582-13.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr02/15/9/anigif_enhanced-buzz-19667-1381844937-10.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26358-1381845043-13.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/9/anigif_enhanced-buzz-18774-1381844645-6.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/9/anigif_enhanced-buzz-25158-1381844793-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/10/anigif_enhanced-buzz-11980-1381846269-1.gif"
+    "https://media4.giphy.com/media/I62KiYQ009SHFLXpUx/giphy.gif?cid=ecf05e474i7wfqu50dx9lv92seiaak5tgvovxoyj3ly57gsi&rid=giphy.gif&ct=g",
+    "https://media1.giphy.com/media/s3SE9MsgbeW3u/giphy.gif?cid=ecf05e4744cd53d8571338432f4506d3caab147b1c95b028&rid=giphy.gif&ct=g",
+    "https://media2.giphy.com/media/GiWEowj3nQv9C/giphy.gif?cid=ecf05e47ondq4vulkvpoady8cp5vwt06nnrtp9haidkioca3&rid=giphy.gif&ct=g",
+    "https://media2.giphy.com/media/jUwpNzg9IcyrK/giphy.gif?cid=ecf05e47r3jlkzvjthzxhrnthj7ba89orjaazizhiuktpuxl&rid=giphy.gif&ct=g",
+    "https://media4.giphy.com/media/fUYhyT9IjftxrxJXcE/giphy.gif?cid=ecf05e47r3jlkzvjthzxhrnthj7ba89orjaazizhiuktpuxl&rid=giphy.gif&ct=g",
+    "https://media1.giphy.com/media/3o6gDWzmAzrpi5DQU8/giphy.gif?cid=ecf05e474i7wfqu50dx9lv92seiaak5tgvovxoyj3ly57gsi&rid=giphy.gif&ct=g",
+    "https://media1.giphy.com/media/3ndAvMC5LFPNMCzq7m/giphy.gif?cid=ecf05e474i7wfqu50dx9lv92seiaak5tgvovxoyj3ly57gsi&rid=giphy.gif&ct=g",
+    "https://media0.giphy.com/media/jKaFXbKyZFja0/giphy.gif?cid=ecf05e47wkzby36tytyf0b4neduyjtasnu975tepic32nnl8&rid=giphy.gif&ct=g",
+    "https://media0.giphy.com/media/byuWzpSHNk7EA/giphy.gif?cid=ecf05e47t51fn2f011tv4dmhg0ig2qygzn4fp3u2cjgwbgtu&rid=giphy.gif&ct=g",
+    "https://media2.giphy.com/media/OPU6wzx8JrHna/giphy.gif?cid=ecf05e47ruhhsj8g9dv0bxwv3g71uwlwr28mqmyc88ue3jof&rid=giphy.gif&ct=g",
+    "https://media2.giphy.com/media/lpHPFVpk65qpbH2XY5/giphy.gif?cid=ecf05e47g26j9lo44wxv932kshj1rmettw5bbmk4zg2bsxtz&rid=giphy.gif&ct=g",
+    "https://media1.giphy.com/media/erwgRIFXjS0zC/giphy.gif?cid=ecf05e474328ov9jpb8vu2u17aw01lgk16z1k5u44insftr0&rid=giphy.gif&ct=g"
 ]
 
 @app.route('/')
